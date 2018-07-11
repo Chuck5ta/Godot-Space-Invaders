b705b89c9e203458c69e700f918f6d988aaace6b
Charles A Edwards <chuckstarrrr@googlemail.com>
2018-07-11 09:42:18 +0100

    Barrier destruction by player and invaders working
    Barrier destruction by player and invaders working correctly

--- TextureRect.gd
+++ TextureRect.gd
@@ -78,6 +78,7 @@ func _blast_barrier(LaserCoords, BarrierCoords, BlastDirection):
             SpriteYLocation = 0
         else:
             SpriteYLocation = (Ydifference / ActualBarrierHeight) * 16 # (truncate?)
+        SpriteYLocation = 0 # TODO delete the above code to do with calculating the Y coord, as ir will always be 0
         #_write_pixel(SpriteXLocation, SpriteYLocation, BlastDirection)
     else:
         var LaserBoltStrikeCoordX = LaserCoords.x
@@ -105,30 +106,58 @@ func _blast_barrier(LaserCoords, BarrierCoords, BlastDirection):
             SpriteYLocation = Ydifference / ActualBarrierHeight * 15 # (truncate?)
         else:
             SpriteYLocation = 15
+        SpriteYLocation = 15 # TODO delete the above code to do with calculating the Y coord, as ir will always be 15
        # _write_pixel(SpriteXLocation, SpriteYLocation, BlastDirection)
-    image.lock()
-    image.set_pixel(round(SpriteXLocation), round(SpriteYLocation), Color(1.0, 0.0, 0.0, 1.0))
-    image.unlock()
-    _write_image_back()
+    
+    _write_pixel(SpriteXLocation, SpriteYLocation, BlastDirection)
         
 func _write_pixel(SpriteXLocation, SpriteYLocation, BlastDirection):
     image.lock()
-    # check if the pixel has been destroyed already
     var pixelColour = image.get_pixel(SpriteXLocation, SpriteYLocation)
     
-    while SpriteYLocation < 15 && pixelColour == destroyedPixel:
-        # need to increase Y
-        SpriteYLocation += 1
-    # kill all pixels along Y
-    var pixelsToDestroy = 6
-    if SpriteXLocation + 6 <= 21:
-        SpriteXLocation += 6
-    for y in SpriteYLocation:
-        #pixelsToDestroy -= 1
-        for pixel in pixelsToDestroy:  
-            # destroy a line of pixels along the X axis    
-            if randi()%11+1 < 8:                
-                image.set_pixel(round(SpriteXLocation + pixel), y, Color(0.0, 0.0, 0.0, 0.0))
+    if BlastDirection == "down":
+        while SpriteYLocation < 15 && pixelColour == destroyedPixel:
+            # need to increase Y
+            SpriteYLocation += 1
+            # check if the pixel has been destroyed already
+            pixelColour = image.get_pixel(SpriteXLocation, SpriteYLocation)
+        # kill all pixels along Y
+        var pixelsToDestroy = 5
+        if SpriteXLocation - 2 >= 0:
+            SpriteXLocation -= 2
+        elif SpriteXLocation - 1 >= 0:
+            SpriteXLocation -= 1
+        var TotalRowsToDestroy = 5
+        while TotalRowsToDestroy > 0:
+            #pixelsToDestroy -= 1
+            for pixel in pixelsToDestroy:  
+                # destroy a line of pixels along the X axis    
+                if randi()%11+1 < 8:                
+                    image.set_pixel(round(SpriteXLocation + pixel), SpriteYLocation, Color(0.0, 0.0, 0.0, 0.0))
+            SpriteYLocation += 1
+            TotalRowsToDestroy -= 1
+    else: # player gun shooting up the screen
+        while SpriteYLocation > 0 && pixelColour == destroyedPixel:
+            # need to increase Y
+            SpriteYLocation -= 1
+            # check if the pixel has been destroyed already
+            pixelColour = image.get_pixel(SpriteXLocation, SpriteYLocation)
+        # kill all pixels along Y
+        var pixelsToDestroy = 5
+        if SpriteXLocation - 2 >= 0:
+            SpriteXLocation -= 2
+        elif SpriteXLocation - 1 >= 0:
+            SpriteXLocation -= 1
+        var TotalRowsToDestroy = 5
+        while TotalRowsToDestroy > 0:
+            #pixelsToDestroy -= 1
+            for pixel in pixelsToDestroy:  
+                # destroy a line of pixels along the X axis    
+                if randi()%11+1 < 8:                
+                    image.set_pixel(round(SpriteXLocation + pixel), SpriteYLocation, Color(0.0, 0.0, 0.0, 0.0))
+            SpriteYLocation -= 1
+            TotalRowsToDestroy -= 1
+        
     image.unlock()
     _write_image_back()
   
@@ -137,5 +166,4 @@ func _write_image_back():
     $BarrierSprite.texture = imageTexture
 
 
-   
-    
+
